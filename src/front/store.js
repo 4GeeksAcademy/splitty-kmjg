da8c32f4a3e0aea160beee2391621c1ab512dd46
Marco Antonio Gonzalez <gartonyx@gmail.com>
2026-03-08 20:35:59 +0000

    Arreglo de migracion y ajuste del backend para iniciar front de login

--- src/front/store.js
+++ src/front/store.js
@@ -1,33 +1,43 @@
-export const initialStore=()=>{
-  return{
-   jwt: localStorage.getItem("token") || null,
-   user: {
-    email: localStorage.getItem("user_email") || null,
-   },
-  }
-}
+export const initialStore = () => {
+  return {
+    jwt: localStorage.getItem("token") || null,
+    user: {
+      email: localStorage.getItem("user_email") || null,
+    },
+    message: null
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'SET_JWT':
+  switch (action.type) {
+    case "SET_JWT":
       return {
         ...store,
         jwt: action.payload
-      }
-    case 'SET_USER':
+      };
+
+    case "SET_USER":
       return {
         ...store,
         user: action.payload
-      }
-    case 'UNSET_USER':
+      };
+
+    case "UNSET_USER":
       return {
         ...store,
         user: {
           email: null
         },
         jwt: null
-      }
+      };
+
+    case "set_hello":
+      return {
+        ...store,
+        message: action.payload
+      };
+
     default:
-      throw Error('Unknown action: ' + action.type);
-  }    
+      throw Error("Unknown action: " + action.type);
+  }
 }
