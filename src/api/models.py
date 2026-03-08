c57bb4d2a8501aa117afc5faeb3dff4641776e86
gusdev <59888502+Gus2708@users.noreply.github.com>
2026-03-08 05:49:31 +0000

    - Añadir soporte para CI en el registro de usuarios y se añadio el ci en el modelo. - inicializacion de el store con los dispatch y action para inicio, registro y logout de usuario - cambio de endpoints al archivo routes.py - ajustes en admin.py para guardar la contraseña hasheada desde el administrador de la base de datos - se integro un endpoint de logout con blockedToken para que no se puedan reutilizar tokens pasados

--- src/api/models.py
+++ src/api/models.py
@@ -1,15 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
-from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    username : Mapped[str] = mapped_column(String(120),nullable=True)
+    username : Mapped[str] = mapped_column(String(120),nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    ci: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -19,5 +19,10 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "username" : self.username
-            # do not serialize the password, its a security breach
         }
+    
+class BlockedToken(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    jti: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    blocked_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.utcnow)
