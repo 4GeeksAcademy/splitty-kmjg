f1aa559f210b189815131366170cd7084443573b
gusdev <59888502+Gus2708@users.noreply.github.com>
2026-03-19 04:20:09 +0000

    solved problem with cors in db

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_cors import CORS
 from flask_mail import Mail
 
 # from models import Person
@@ -20,6 +21,7 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+CORS(app)
 
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
