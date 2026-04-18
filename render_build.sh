4e047baabe0fa1e2cefab9921c561b766378330e
Scary Gamer <scarygamer08@gmail.com>
2026-04-18 00:43:57 -0400

    fix(render): replace pipenv with pip for Render deployment

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Run database migrations
+python -m flask db upgrade
+
+# Install Node dependencies and build frontend
 npm install
 npm run build
-
-pipenv install
-
-pipenv run upgrade
